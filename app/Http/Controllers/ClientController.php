9e265ba0ac0166470a2076529c33647979d9f5ce
Guilherme Johann <guilherme4695@gmail.com>
2026-01-30 21:43:47 -0300

    feat: adição dos endpoints restantes

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -22,4 +22,77 @@ class ClientController extends Controller
         $clients = Client::paginate(10);
         return response()->json($clients, 200);
     }
+
+    public function clientById(string $id, Request $request)
+    {
+        try {
+            $client = Client::findOrFail($id);
+            return response()->json($client, 200);
+        } catch (\Throwable $exception) {
+            return response()->json([
+                'path'=> $request->getRequestUri(),
+                'method' => $request->getMethod(),
+                'status' => 404,
+                'message' => "Couldn't find the client with id {$id}",
+            ], 404);
+        }
+
+    }
+
+    public function createClient(Request $request)
+    {
+            $validatedData = $request->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique:clients',
+            ]);
+
+            $client = Client::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+            ]);
+
+            return response()->json($client, 201);
+    }
+
+    public function updateClient(string $id, Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        try {
+
+            $client = Client::findOrFail($id);
+            $client->name = $validatedData['name'];
+            $client->email = $validatedData['email'];
+            $client->save();
+
+            return response()->json($client, 200);
+        } catch (\Throwable $exception) {
+            return response()->json([
+                'path'=> $request->getRequestUri(),
+                'method' => $request->getMethod(),
+                'status' => 404,
+                'message' => "Couldn't find the client with id {$id}",
+            ], 404);
+        }
+    }
+
+    public function deleteClient(string $id, Request $request)
+    {
+        try {
+            $client = Client::findOrFail($id);
+            $client->delete();
+            return response()->json($client, 200);
+        } catch (\Throwable $exception) {
+            return response()->json([
+                'path'=> $request->getRequestUri(),
+                'method' => $request->getMethod(),
+                'status' => 404,
+                'message' => "Couldn't find the client with id {$id}",
+            ], 404);
+        }
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/status', [ClientController::class, 'status']);
 Route::get('/clients', [ClientController::class, 'clients']);
+Route::get('/clients/{id}', [ClientController::class, 'clientById']);
+Route::post('/clients', [ClientController::class, 'createClient']);
+Route::put('/clients/{id}', [ClientController::class, 'updateClient']);
+Route::delete('/clients/{id}', [ClientController::class, 'deleteClient']);
